ed13f7a076916bf0460ccc9390c7854f7f89ab10
Maxym <so.unique.december@gmail.com>
2024-04-03 17:04:19 +0300

    init commit

---
+++ src/main/java/com/telegram/maxym/telbot/TelBotCurrencyWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.telegram.maxym.telbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelBotCurrencyWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelBotCurrencyWeatherApplication.class, args);
+    }
+
+}
